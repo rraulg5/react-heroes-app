7bbf5726b039937fc4f85ba8bb828e75f3879092
Raul Garcia <rraulg5@gmail.com>
2026-03-29 17:07:39 -0600

    feat: restructure appRouter to include HeroesLayout and AdminLayout with nested routes

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,24 +1,39 @@
+import { createBrowserRouter } from 'react-router';
+
+import { AdminLayout } from '@/admin/pages/layouts/AdminLayout';
 import { AdminPage } from '@/admin/pages/AdminPage';
+import { HeroesLayout } from '@/heroes/pages/hero/layouts/HeroesLayout';
 import { HeroPage } from '@/heroes/pages/hero/HeroPage';
 import { HomePage } from '@/heroes/pages/home/HomePage';
 import { SearchPage } from '@/heroes/pages/search/SearchPage';
-import { createBrowserRouter } from 'react-router';
 
 export const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/hero',
-    element: <HeroPage />,
-  },
-  {
-    path: '/search',
-    element: <SearchPage />,
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/hero',
+        element: <HeroPage />,
+      },
+      {
+        path: '/search',
+        element: <SearchPage />,
+      },
+    ],
   },
   {
     path: '/admin',
-    element: <AdminPage />,
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />,
+      },
+    ],
   },
 ]);

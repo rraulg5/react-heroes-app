4eaf824655157bac6e6b216b706fe811700e8bb5
Raul Garcia <rraulg5@gmail.com>
2026-03-30 21:47:51 -0600

    feat: implement SearchPage with MyJumbotron and HeroStats components for enhanced user experience

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,3 +1,17 @@
+import { HeroStats } from '@/heroes/components/HeroStats';
+import { MyJumbotron } from '@/components/custom/MyJumbotron';
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <>
+      {/* Header */}
+      <MyJumbotron
+        title='Search your hero'
+        description='Find your favorite hero'
+      />
+
+      {/* Stats Dashboard */}
+      <HeroStats />
+    </>
+  );
 };

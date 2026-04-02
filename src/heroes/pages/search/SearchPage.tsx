3fa6092d49d4bce0e52df0e77397bdfc4475edad
Raul Garcia <rraulg5@gmail.com>
2026-04-02 12:40:04 -0600

    feat: update SearchPage to include SearchControls component

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,6 @@
 import { HeroStats } from '@/heroes/components/HeroStats';
 import { MyJumbotron } from '@/components/custom/MyJumbotron';
+import { SearchControls } from './ui/SearchControls';
 
 export const SearchPage = () => {
   return (
@@ -12,6 +13,9 @@ export const SearchPage = () => {
 
       {/* Stats Dashboard */}
       <HeroStats />
+
+      {/* Search Controls */}
+      <SearchControls />
     </>
   );
 };
